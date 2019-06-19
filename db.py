8ebcaa4ae2177d42e8e4f9d026b70d8d2fc9b503
Michael <bostwicm@gmail.com>
2019-06-18 23:08:03 -0600

    Updated tables to reflect model changes

--- db.py
+++ db.py
@@ -6,14 +6,14 @@ def connect(db):
 
 def create_tables(conn):
     c = conn.cursor()
-    c.execute('''CREATE TABLE IF NOT EXISTS publications (id INTEGER PRIMARY KEY, title TEXT, abstract TEXT, FOREIGN KEY (id) REFERENCES snps (rsid))''')
-    c.execute('''CREATE TABLE IF NOT EXISTS snps (rsid INTEGER PRIMARY KEY, publication_id INTEGER, FOREIGN KEY (publication_id) REFERENCES publications (id))''')
+    c.execute('''CREATE TABLE IF NOT EXISTS publications (id INTEGER PRIMARY KEY, title TEXT, abstract TEXT, FOREIGN KEY (id) REFERENCES snps_publications (rsid))''')
+    c.execute('''CREATE TABLE IF NOT EXISTS snps_publications (rsid INTEGER, publication_id INTEGER, FOREIGN KEY (publication_id) REFERENCES publications (id))''')
     conn.commit()
     return()
 
 def add_snp(conn, rsid, publication_id):
     c = conn.cursor()
-    c.execute("INSERT INTO snps VALUES (?, ?)", (rsid, publication_id))
+    c.execute("INSERT INTO snps_publications VALUES (?, ?)", (rsid, publication_id))
     conn.commit()
     return()
 
