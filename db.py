21aa02e2e0893fe26e7f7ff2ae17f30640fbc8b0
Jonathan DeMasi <jonathan.demasi@colorado.edu>
2019-06-18 22:39:48 -0600

    added empty returns, put commits in each function to ensure we don't miss changes

--- db.py
+++ db.py
@@ -1,23 +1,27 @@
 import sqlite3
-def establishConnection(db):
+
+def connect(db):
     conn = sqlite3.connect(db)
 
-def createTables(conn):
+def create_tables(conn):
     c = conn.cursor()
-
     c.execute('''CREATE TABLE IF NOT EXISTS publications (id INTEGER PRIMARY KEY, title TEXT, abstract TEXT, FOREIGN KEY (id) REFERENCES snps (rsid))''')
     c.execute('''CREATE TABLE IF NOT EXISTS snps (rsid INTEGER PRIMARY KEY, publication_id INTEGER, FOREIGN KEY (publication_id) REFERENCES publications (id))''')
-
-def addSnpsRow(conn, rsid, publication_id):
-    c = conn.cursor()
-
-    c.execute("INSERT INTO snps VALUES (?, ?)", (rsid, publication_id))
-
-def addPublicationsRow(conn, id, title, abstract):
-    c = conn.cursor()
-
-    c.execute("INSERT INTO publications VALUES (?, ?, ?)", (id, title, abstract))
-
-def commitAndClose(conn):
     conn.commit()
+    return()
+
+def add_snp(conn, rsid, publication_id):
+    c = conn.cursor()
+    c.execute("INSERT INTO snps VALUES (?, ?)", (rsid, publication_id))
+    conn.commit()
+    return()
+
+def add_publication(conn, id, title, abstract):
+    c = conn.cursor()
+    c.execute("INSERT INTO publications VALUES (?, ?, ?)", (id, title, abstract))
+    conn.commit()
+    return()
+
+def close(conn):
     conn.close()
+    return()
