ee344281fef3c68270b76c28d50b693139c20dcf
Michael <bostwicm@gmail.com>
2019-06-18 23:59:12 -0600

    Added functionality to ignore duplicate rows

--- db.py
+++ db.py
@@ -7,19 +7,20 @@ def connect(db):
 def create_tables(conn):
     c = conn.cursor()
     c.execute('''CREATE TABLE IF NOT EXISTS publications (id INTEGER PRIMARY KEY, title TEXT, abstract TEXT, FOREIGN KEY (id) REFERENCES snps_publications (rsid))''')
-    c.execute('''CREATE TABLE IF NOT EXISTS snps_publications (rsid INTEGER, publication_id INTEGER, FOREIGN KEY (publication_id) REFERENCES publications (id))''')
+    c.execute('''CREATE TABLE IF NOT EXISTS snps_publications (rsid INTEGER, publication_id INTEGER, FOREIGN KEY (publication_id) REFERENCES publications (id), UNIQUE(rsid, publication_id))''')
     conn.commit()
     return()
 
 def add_snp(conn, rsid, publication_id):
     c = conn.cursor()
-    c.execute("INSERT INTO snps_publications VALUES (?, ?)", (rsid, publication_id))
+
+    c.execute("INSERT OR IGNORE INTO snps_publications VALUES (?, ?)", (rsid, publication_id))
     conn.commit()
     return()
 
 def add_publication(conn, id, title, abstract):
     c = conn.cursor()
-    c.execute("INSERT INTO publications VALUES (?, ?, ?)", (id, title, abstract))
+    c.execute("INSERT OR IGNORE INTO publications VALUES (?, ?, ?)", (id, title, abstract))
     conn.commit()
     return()
 
