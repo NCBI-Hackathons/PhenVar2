e6e0278b39e890be601cfaa8a18a8672d00d7579
Jonathan DeMasi <jonathan.demasi@colorado.edu>
2019-06-18 22:03:21 -0600

    only create Snp objects for rsids that are cited in pubmed

--- phenvar.py
+++ phenvar.py
@@ -2,13 +2,15 @@
 
 import ncbiutils
 from lxml import etree
+import snp
+import publication
 
 """
 Finds all rsids that are explicitly cited in pubmed
 and returns a list
 """
 def get_complete_rsids():
-    results = ncbiutils.esearch(db="snp", retmode="json", retmax=200000, retstart=0, term='snp_pubmed_cited[sb]')
+    results = ncbiutils.esearch(db="snp", retmode="json", retmax=200000, retstart=0, term='snp_pubmed_cited[sb]', api_key="7c0213f7c513fa71fe2cb65b4dfefa76fb09")
     rsidlist = results["esearchresult"]["idlist"]
     for x in range(0, len(rsidlist)):
         rsidlist[x] = "rs" + rsidlist[x]
@@ -28,19 +30,34 @@ def get_pmids(rsid):
 Takes a pmid and returns the abstract text
 """
 def get_abstract(pmid):
-    raw = ncbiutils.efetch(db="pubmed", id=pmid, rettype="abstract")
+    raw = ncbiutils.efetch(db="pubmed", id=pmid, rettype="abstract", api_key="7c0213f7c513fa71fe2cb65b4dfefa76fb09")
     xml = etree.fromstring(raw)
     abstracts = []
     for a in xml.xpath('//AbstractText'):
         abstracts.append(a.text)
+    titles = []
+    for t in xml.xpath('//ArticleTitle'):
+        titles.append(t.text)
+    print(titles)
     return()
 
-def main():
+"""
+Initializes Snp objects and creates a list of them.  This list
+can be later used to initilize a database or other magic
+"""
+def init_rsids():
+    snps = []
     rsids = get_complete_rsids()
     for rsid in rsids:
         pmids = get_pmids(rsid)
-        for pmid in pmids:
-            get_abstract(pmid)
+        if len(pmids) > 0:
+            a_snp = snp.Snp(id=rsid, publications=pmids)
+            snps.append(a_snp)
+        print(len(snps))
+    return(snps)
+        
+def main():
+    snps = init_rsids()
     return()
 
 if __name__ == '__main__':
---
+++ snp.py
@@ -0,0 +1,4 @@
+class Snp:
+    def __init__(self, id, publications=[]):
+        self.id = id
+        self.publications = publications
