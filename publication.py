1d557bd2cf0c43aa9ea78a25154b2700409bc24a
Jonathan DeMasi <jonathan.demasi@colorado.edu>
2019-06-20 12:04:12 -0600

    removing classes

--- publication.py
+++
@@ -1,6 +0,0 @@
-class Publication:
-    def __init__(self, id, title, abstract, rsids=[]):
-        self.id = id
-        self.title = title
-        self.abstract = abstract
-        self.rsids = rsids
--- snp.py
+++
@@ -1,4 +0,0 @@
-class Snp:
-    def __init__(self, id, publications=[]):
-        self.id = id
-        self.publications = publications
